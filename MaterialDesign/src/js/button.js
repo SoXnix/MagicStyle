a89442fb88a93620e7b96fda305c1d1cd9e4231c
Rainey <515385036@qq.com>
2016-06-30 15:32:38 +0800

    增加图标按钮

--- MaterialDesign/src/js/button.js
+++ MaterialDesign/src/js/button.js
@@ -1,23 +1,45 @@
+var getEventTarget = function (e) {
+  var eventPathTagName = e.path.map(function(item) {
+    return item.tagName;
+  })
+  return e.path[eventPathTagName.indexOf('BUTTON')];
+}
+
 var addRippleEffect = function (e) {
-    var rippleEffectTag=['button'];
-    var target = e.target;
-    if(rippleEffectTag.indexOf(target.tagName.toLowerCase())<0) return false;
-
-    var rect = target.getBoundingClientRect();
-    var ripple = target.querySelector('.ripple');
-
-    if (!ripple) {
-        ripple = document.createElement('span');
-        ripple.className = 'ripple';
-        ripple.style.height = ripple.style.width = Math.max(rect.width, rect.height) + 'px';
-        target.appendChild(ripple);
+    //返回事件冒泡流中的button元素
+    var target = getEventTarget(e)
+    if(!target){
+      return false;
     }
+
+    //获取当前元素的二维数据
+    var rect = target.getBoundingClientRect();
+    //创建ripple
+
+    var ripple = document.createElement('span');
+    ripple.className = 'ripple';
+    ripple.style.height = ripple.style.width = Math.max(rect.width, rect.height) + 'px';
+    target.appendChild(ripple);
+
+    //当动画结束时销毁ripple
+    ripple.addEventListener("animationend", function() {
+      ripple.parentNode.removeChild(ripple);
+    }, false);
     ripple.classList.remove('show');
-    var top = e.pageY - rect.top - ripple.offsetHeight / 2 - document.body.scrollTop;
-    var left = e.pageX - rect.left - ripple.offsetWidth / 2 - document.body.scrollLeft;
+
+    //ripple在父元素中的位置计算
+    if(target.className.split(" ").indexOf('button-outline')>0){
+      var top = (rect.height/2) - (ripple.offsetHeight/2)
+      var left = 0;
+    }else{
+      var top = e.pageY - rect.top - ripple.offsetHeight / 2 - document.body.scrollTop;
+      var left = e.pageX - rect.left - ripple.offsetWidth / 2 - document.body.scrollLeft;
+    }
+
     ripple.style.top = top + 'px';
     ripple.style.left = left + 'px';
     ripple.classList.add('show');
+
     return false;
 }
 
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Magic Style
 
 A magic style
+
+## materialDesign
+
+该项目font-icon依赖于[fontawesome](http://fontawesome.io/)
